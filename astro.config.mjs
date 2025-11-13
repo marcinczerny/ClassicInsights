0ccd5a41ef5118a0e0ba4f801480a5f48a0b0ae0
Marcin Czerny <marcin_adm@solutions4delivery.com>
2025-11-13 20:21:54 +0100

    fix: fixed problems with main.yml and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,15 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://classicinsights.pages.dev",
   output: "server",
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: cloudflare({
-    mode: "directory",
-  }),
+  devToolbar: {
+    enabled: false,
+  },
+  adapter: cloudflare(),
 });
